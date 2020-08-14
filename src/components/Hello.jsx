95578b3f30067234a88403d95df887844a2c3a89
jracosta29 <60927822+jracosta29@users.noreply.github.com>
2020-08-14 13:25:39 -0500

    Eslint

---
+++ src/components/Hello.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+class Hello extends React.Component {
+ render(){
+     return(
+         <div>Hello</div>
+     )
+ }
+}
+
+export default Hello;
